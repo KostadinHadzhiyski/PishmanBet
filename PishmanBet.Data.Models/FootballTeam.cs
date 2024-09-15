a7b8f4de824dd8f1c465b6538a666a1d0878588b
Kostadin Hadzhiyski <KostadinHadzhiyski@gmail.com>
2024-09-15 20:12:43 +0300

    Added matches and teams in Database

---
+++ PishmanBet.Data.Models/FootballTeam.cs
@@ -0,0 +1,30 @@
+﻿
+namespace PishmanBet.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static PishmanBet.Common.ValidationConstants.FootballTeam;
+    public class FootballTeam
+    {
+        public FootballTeam()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(FootballTeamNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(FootballTeamOddNameMaxLength)]
+        public string OddName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(FootballTeamScoreNameMaxLength)]
+        public string ScoreName { get; set; } = null!;
+
+        public ICollection<FootballMatch> Matches { get; set; } = new HashSet<FootballMatch>();
+    }
+}
--- PishmanBet.Data/PishmanBetDbContext.cs
+++ PishmanBet.Data/PishmanBetDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PishmanBet.Data.Models;
 
 namespace PishmanBet.Data
 {
@@ -8,6 +9,25 @@ namespace PishmanBet.Data
         public PishmanBetDbContext(DbContextOptions<PishmanBetDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<FootballTeam> Teams { get; set; }
+        public DbSet<FootballMatch> Matches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<FootballMatch>()
+                .HasOne(fm => fm.AwayTeam)
+                .WithMany()
+                .HasForeignKey(fm => fm.AwayTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<FootballMatch>()
+              .HasOne(fm => fm.HomeTeam)
+              .WithMany()
+              .HasForeignKey(fm => fm.HomeTeamId)
+              .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
